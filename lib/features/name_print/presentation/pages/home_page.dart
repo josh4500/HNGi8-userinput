a14cf53ed7824d26a0ad6d7854cc5bd15a33fc39
josh4500 <jibo.ajosh45@gmail.com>
2021-08-22 13:54:04 +0100

    Change UI

--- lib/features/name_print/presentation/pages/home_page.dart
+++ lib/features/name_print/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -17,14 +18,111 @@ class _HomePageState extends State<HomePage> {
     super.initState();
   }
 
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            child: new Wrap(
+              children: <Widget>[
+                ListTile(
+                  leading: Image.asset(
+                    "assets/images/github-logo.png",
+                    height: 24,
+                    width: 24,
+                  ),
+                  title: new Text('Built by Ajibola'),
+                  onTap: () async {
+                    if (await canLaunch("https://github.com/josh4500")) {
+                      await launch("https:github.com/josh4500");
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    "assets/images/logo.png",
+                    height: 24,
+                    width: 24,
+                  ),
+                  title: Text('For HNG Intership 8'),
+                  onTap: () async {
+                    if (await canLaunch("https://github.com/josh4500")) {
+                      await launch("https:github.com/josh4500");
+                    }
+                  },
+                ),
+                ListTile(
+                  leading: Image.asset(
+                    "assets/images/code-img.png",
+                    height: 24,
+                    width: 24,
+                  ),
+                  title: Text(
+                    'Check the code at',
+                    style: TextStyle(
+                      decoration: TextDecoration.underline,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  onTap: () async {
+                    if (await canLaunch(
+                        "https//:github.com/josh4500/HNGi8-userinput")) {
+                      await launch("https:github.com/josh4500");
+                    }
+                  },
+                ),
+              ],
+            ),
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
+    var _size = MediaQuery.of(context).size;
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          title: Text("HNGi8"),
-          backgroundColor: Colors.black,
+        appBar: PreferredSize(
+          preferredSize: Size(_size.width - 30, 70),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: AppBar(
+              leading: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset(
+                  "assets/images/logo.png",
+                  fit: BoxFit.contain,
+                ),
+              ),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              title: Text(
+                "HNGi8 - Internship",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black54,
+                ),
+              ),
+              elevation: 16.0,
+              shadowColor: Colors.black,
+              backgroundColor: Colors.white.withOpacity(.9),
+              actions: [
+                IconButton(
+                  icon: Icon(
+                    Icons.more_vert_outlined,
+                    color: Colors.black,
+                  ),
+                  onPressed: () {
+                    _showBottomSheet(context);
+                  },
+                )
+              ],
+            ),
+          ),
         ),
+        backgroundColor: Color(0xFF63C877),
         body: GestureDetector(
           onTap: () {
             FocusScope.of(context).unfocus();
@@ -32,37 +130,19 @@ class _HomePageState extends State<HomePage> {
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Expanded(
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 8.0,
-                      horizontal: 4.0,
-                    ),
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.black87,
-                          width: 2,
-                        ),
-                        borderRadius: BorderRadius.circular(8.0)),
-                    child: SingleChildScrollView(
-                      child: Container(
-                        child: Text(
-                          userText.length > 0
-                              ? "$userText"
-                              : "The display text is shown here...",
-                          style: userText.length > 0
-                              ? TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w400,
-                                )
-                              : TextStyle(
-                                  color: Colors.grey,
-                                  fontSize: 16,
-                                ),
-                        ),
-                      ),
+                Container(
+                  height: 40.0,
+                  child: Text(
+                    userText.length > 0
+                        ? "$userText"
+                        : "The display text is shown here...",
+                    style: TextStyle(
+                      color:
+                          userText.length > 0 ? Colors.white : Colors.white54,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
                 ),
@@ -73,10 +153,7 @@ class _HomePageState extends State<HomePage> {
                   padding: const EdgeInsets.symmetric(vertical: 8.0),
                   child: Container(
                     decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black87,
-                        width: 2,
-                      ),
+                      color: Colors.white24,
                       borderRadius: BorderRadius.circular(8.0),
                     ),
                     child: TextField(
@@ -97,6 +174,16 @@ class _HomePageState extends State<HomePage> {
                             Icons.close_outlined,
                           ),
                         ),
+                        hintStyle: TextStyle(
+                          color: Colors.white70,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      cursorColor: Colors.white12,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
                       ),
                     ),
                   ),
@@ -119,20 +206,34 @@ class _HomePageState extends State<HomePage> {
                       Size(double.infinity, 60),
                     ),
                     tapTargetSize: MaterialTapTargetSize.padded,
-                    foregroundColor: MaterialStateProperty.all(Colors.white),
-                    backgroundColor:
-                        MaterialStateProperty.all(Colors.blueAccent),
+                    foregroundColor: MaterialStateProperty.all(Colors.green),
+                    backgroundColor: MaterialStateProperty.all(Colors.white),
+                    shadowColor: MaterialStateProperty.all(Colors.black54),
+                    elevation: MaterialStateProperty.all(16.0),
                   ),
-                  child: Container(
+                  child: FittedBox(
                     child: Text(
-                      "Display",
+                      "Display Text",
                       style: TextStyle(
                         fontWeight: FontWeight.w700,
-                        fontSize: 16,
+                        fontSize: 18,
                       ),
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 32.0,
+                ),
+                SizedBox(
+                  child: Container(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      "assets/images/hng-brand-logo.png",
+                      scale: 1.8,
+                      color: Colors.black54,
+                    ),
+                  ),
+                )
               ],
             ),
           ),
